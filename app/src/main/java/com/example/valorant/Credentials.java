881e4a4b8533648811bd01b3a4e3257e15b4cb4f
Jackson <jackson.mclane20@gmail.com>
2020-05-01 19:13:27 -0700

    added backendless dependency added login + registration activities added credentials class backendless database not properly configured

---
+++ app/src/main/java/com/example/valorant/Credentials.java
@@ -0,0 +1,6 @@
+package com.example.valorant;
+
+public class Credentials {
+    public static final String API_KEY = "E2B07278-E4A8-4085-865D-D7C6493A2A33";
+    public static final String APP_ID = "3E84AA29-849A-47EF-81AA-80C628BF1C83";
+}
