91f992c3d2d8b4927f4094059ec3210130090b4e
Jackson <jackson.mclane20@gmail.com>
2020-05-08 18:31:26 -0700

    added user class

--- app/src/main/java/com/example/valorant/Credentials.java
+++ app/src/main/java/com/example/valorant/Credentials.java
@@ -1,6 +1,6 @@
 package com.example.valorant;
 
 public class Credentials {
-    public static final String API_KEY = "E2B07278-E4A8-4085-865D-D7C6493A2A33";
-    public static final String APP_ID = "3E84AA29-849A-47EF-81AA-80C628BF1C83";
+    public static final String API_KEY = "F9C0966D-3279-4D34-B445-77B213089872";
+    public static final String APP_ID = "98B47151-0DB4-1E2B-FF12-6FEAB2463E00";
 }
---
+++ app/src/main/java/com/example/valorant/User.java
@@ -0,0 +1,11 @@
+package com.example.valorant;
+
+public class User {
+    private String username;
+    private int aimRating;
+    private int gamesenseRating;
+    private int communicationRating;
+    private String email;
+    private String objectId;
+    private String ownerId;
+}
