b5eed750f6779ec7054fd5bb503e5f8e2bc7d074
Jackson <jackson.mclane20@gmail.com>
2020-05-15 20:44:21 -0700

    wired widgets for Profile Fragment

--- app/src/main/java/com/example/valorant/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/valorant/ui/profile/ProfileFragment.java
@@ -4,22 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.valorant.R;
 
 public class ProfileFragment extends Fragment {
 
+    private ImageView imageViewProfilePhoto;
+    private TextView textViewUsername;
+    private TextView textViewAimHeader;
+    private RatingBar ratingBarAim;
+    private TextView textViewGamesenseHeader;
+    private RatingBar ratingBarGamesense;
+    private TextView textViewCommunication;
+    private RatingBar ratingBarCommunication;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        return root;
+        wireWidgets(rootView);
+
+        return rootView;
+    }
+
+    private void wireWidgets(View rootView) {
+        imageViewProfilePhoto = rootView.findViewById(R.id.imageView_profile_picture);
+        textViewUsername = rootView.findViewById(R.id.textView_profile_username);
+        textViewAimHeader = rootView.findViewById(R.id.textView_profile_aim_header);
+        ratingBarAim = rootView.findViewById(R.id.ratingBar_profile_aim);
+        textViewGamesenseHeader = rootView.findViewById(R.id.textView_profile_gamesense_header);
+        ratingBarGamesense = rootView.findViewById(R.id.ratingBar_profile_gamesense);
+        textViewCommunication = rootView.findViewById(R.id.textView_profile_communication_header);
+        ratingBarCommunication = rootView.findViewById(R.id.ratingBar_profile_communication);
+
+        //prevents mouse/tabbing into the rating bar
+        ratingBarAim.setFocusable(false);
+        ratingBarGamesense.setFocusable(false);
+        ratingBarCommunication.setFocusable(false);
+        //prevents user from changing their own ratings
+        ratingBarAim.setIsIndicator(true);
+        ratingBarGamesense.setIsIndicator(true);
+        ratingBarCommunication.setIsIndicator(true);
     }
 }
